9c6e863a1e2764c254cb5e6c3227ae6f340430ca
jgao <aus.jacy@gmail.com>
2020-12-16 23:54:08 +1100

    added player and mob

--- Player.gd
+++ Player.gd
@@ -39,3 +39,14 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
+
+
+func _on_Player_body_entered(body):
+	hide()  # Player disappears after being hit.
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
